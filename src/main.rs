c0a448d540493866a179d667091ce297746114d0
Ryan Maugin <119737827+maugzeth@users.noreply.github.com>
2023-12-29 12:21:07 +0400

    2ND-STAGE

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, io::{Read, Write}};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -6,8 +6,14 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut s) => {
                 println!("accepted new connection");
+                let mut buf: Vec<u8> = Vec::new();
+                match s.read(buf.as_mut_slice()) {
+                    Ok(_) =>  println!("Buffer: {:?}", buf),
+                    Err(_) => panic!("failed to read connection data into buffer"),
+                };
+                s.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
